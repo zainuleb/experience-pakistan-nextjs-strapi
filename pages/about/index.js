c979cc181cab4397fca43f54464ef7360ea88dd0
zainuleb <p166024@nu.edu.pk>
2021-11-03 18:50:02 +0500

    Pages and Routing

---
+++ pages/about/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Pakistan is a destination for every one but is very little visited by
+        the world due to bad image from the media. You can rely on Travel n
+        Culture Services in Pakistan to show you this fabulous country. Ours is
+        an owner managed company that takes every care to make your stay in
+        Pakistan comfortable and hassle free. Our main aim is to provide quality
+        service to you at a very reasonable cost. We promote Pakistan as a safe
+        and the most enjoyable secrets of Asia. Our tours are specially designed
+        to give comfort and a variety of things to do. We take special care of
+        our clients during their Travel in Pakistan, by providing them a very
+        personal touch. Our Guides and Tour managers are trained to form a very
+        friendly relation with tourists.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/events/addEvent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddEvent = () => {
+  return (
+    <div>
+      <h1>Add Events</h1>
+    </div>
+  );
+};
+
+export default AddEvent;
